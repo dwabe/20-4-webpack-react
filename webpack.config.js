b2fffabf81febc204b683e6e6d6070833aa0246d
Bartek Błęcki <bartek@blecki.pl>
2019-07-06 12:00:58 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 //webpack.config.js
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
